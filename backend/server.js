cabab78013ce675eada6f99da741794b6a8813e4
abodihb22m2-spec <AbodiHB22m2@gmail.com>
2026-09-08 17:24:18 +0300

    Prepare project for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,26 @@
 import express from "express";
-import cors from 'cors';
-import dotenv from 'dotenv';
-import OpenAI from 'openai';
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import employeeRoutes from "./routes/employeeRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import joinRequestRoutes from "./routes/joinRequestRoutes.js";
 import ticketRoutes from "./routes/ticketRoutes.js";
 import connectDB from "./config/db.js";
-import cookieParser from 'cookie-parser';
 
 dotenv.config();
+
 const app = express();
-
 const PORT = process.env.PORT || 5001;
-app.use(express.json());
 
-connectDB();
+app.use(express.json());
 app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://localhost:5174"
+  "http://localhost:5174",
+  process.env.CLIENT_URL,
 ];
 
 app.use(
@@ -37,16 +37,16 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.send("Backend🚀");
+  res.send("Backend 🚀");
 });
-
-console.log(process.env.OPENROUTER_API_KEY);
 
 app.use("/api/authentication", employeeRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/join-request", joinRequestRoutes);
 app.use("/api/submit", ticketRoutes);
 
+connectDB();
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
